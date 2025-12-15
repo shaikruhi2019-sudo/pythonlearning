8154f282c2592f5ae3a0ea1b102c7258825dfda7
shaikruhi2019-sudo <shaikruhi2019@gmail.com>
2025-12-15 21:43:36 +0530

    Create day4_conditions.py

---
+++ day4_conditions.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+print("\nNumbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
