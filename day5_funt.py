fa7191790f1d09e4151a0ce1e8101e24ad326b46
shaikruhi2019-sudo <shaikruhi2019@gmail.com>
2025-12-16 20:20:30 +0530

    Create day5_funt.py

---
+++ day5_funt.py
@@ -0,0 +1,13 @@
+def student_info(name, branch):
+    print("\nStudent Information")
+    print("Name:", name)
+    print("Branch:", branch)
+
+student_info("Ruhi", "CSE")
+
+
+def add_numbers(a, b):
+    return a + b
+
+sum = add_numbers(10, 20)
+print("Sum is:", sum)
